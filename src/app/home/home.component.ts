0c7f991688989e20a7d22638cd427799462fa30b
alok mondal <alok.webideasolution@gmail.com>
2021-01-18 13:01:54 +0530

    there

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -43,9 +43,11 @@ export class HomeComponent implements OnInit {
   getCart(): void {
     this.cart = JSON.parse(localStorage.getItem('cart'));
     this.quantity = 0;
-    this.cart.items.forEach(element => {
-      this.quantity = this.quantity + element.quantity;
-    });
+    if (this.cart !== null) {
+      this.cart.items.forEach(element => {
+        this.quantity = this.quantity + element.quantity;
+      });
+    }
   }
 
   goToCate(item) {
